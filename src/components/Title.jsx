92f0489fee30bce2524c871c15a2fc20dfae6078
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-06 12:45:58 +0200

    props refactoring

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,8 +5,8 @@ const StyledTitle = styled.h1`
     color: ${(props) => props.color};
 `;
 
-const Title = ({ children, color }) => {
-    return <StyledTitle color={color}>{children}</StyledTitle>;
+const Title = (props) => {
+    return <StyledTitle {...props} />;
 };
 
 export default Title;
