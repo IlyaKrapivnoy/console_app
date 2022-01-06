7d40a552f1caa13fd994287769a04077428c59d1
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-06 12:32:19 +0200

    AppWrapper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    padding: 2rem;
+    background: black;
+`;
+
 function App() {
-    return <div></div>;
+    return <AppWrapper>test</AppWrapper>;
 }
 
 export default App;
